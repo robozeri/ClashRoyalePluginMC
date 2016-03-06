8ed4db1c897f9026dbfba2c99998ac8310b78241
robozeri <robozerirobot@gmail.com>
2016-03-06 13:07:26 +0100

    Update ClashRoyale.java

--- src/main/java/me/robozeri/ClashRoyale.java
+++ src/main/java/me/robozeri/ClashRoyale.java
@@ -1,1 +1,21 @@
+package me.robozeri;
 
+import me.robozeri.commands.ArenaCommand;
+import me.robozeri.commands.ClanCommand;
+import me.robozeri.commands.CardCommand;
+import me.robozeri.commands.RoyaleCommand;
+import me.robozeri.commands.ShopCommand;
+import me.robozeri.lib.database;
+/**
+  _______         __   ___                 __   
+ / ___/ /__ ____ / /  / _ \___  __ _____ _/ /__ 
+/ /__/ / _ `(_-</ _ \/ , _/ _ \/ // / _ `/ / -_)
+\___/_/\_,_/___/_//_/_/|_|\___/\_, /\_,_/_/\__/ 
+ */
+public class ClashRoyale extends JavaPlugin{
+  
+  
+  
+  
+  
+}
